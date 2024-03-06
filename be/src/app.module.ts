f34559b203bd0f820f1154fb23f1ae7786ee9d4a
youseock <mahwin7085@gmail.com>
2024-03-06 23:34:37 +0900

    feat: .env 설정 변경

--- be/src/app.module.ts
+++ be/src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    PostsModule,
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
