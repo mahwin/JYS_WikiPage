808de43a38d7fb0e84221621017cd1bd7196f4d0
youseock <mahwin7085@gmail.com>
2024-03-08 16:51:23 +0900

    chore: console 삭제

--- be/src/main.ts
+++ be/src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(process.env.CLIENT_URL);
   app.enableCors({
     origin: process.env.CLIENT_URL,
     methods: 'GET,PATCH,POST,DELETE',
